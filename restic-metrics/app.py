447021f42f10ebdd3dbd5a069545735d943c93f0
Patrick Ruckstuhl <patrick@ch.tario.org>
2018-05-29 20:48:12 +0200

    switch to gauge

--- restic-metrics/app.py
+++ restic-metrics/app.py
@@ -1,19 +1,16 @@
-from prometheus_client import start_http_server, Summary
-import random
+from prometheus_client import start_http_server, Gauge
 import time
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
-# Decorate function with metric.
-@REQUEST_TIME.time()
-def process_request(t):
-    """A dummy function that takes some time."""
-    time.sleep(t)
+def retrieveLastBackupTime():
+    return 2
 
 if __name__ == '__main__':
+    print('foo')
+    timeLastBackup = Gauge('restic_backup_last_completed_time_seconds', 'Timestamp of the last backup')
+    timeLastBackup.set_function(retrieveLastBackupTime)
+
     # Start up the server to expose the metrics.
     start_http_server(8000)
-    # Generate some requests.
+
     while True:
-        process_request(random.random())
+        time.sleep(10)
